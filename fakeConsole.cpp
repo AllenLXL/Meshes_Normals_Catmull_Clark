d463d352bef1a5eb1060efb4537e5ec838a47b37
allenlxl <allenlxl1126@gmail.com>
2019-02-18 11:42:28 -0500

    obj done

---
+++ fakeConsole.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Allen Li on 2019-02-17.
+//
+
+#include "shared/eigen/Eigen/Core"
+#include "shared/eigen/Eigen/Dense"
+#include "shared/eigen/Eigen/Geometry"
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+//  Matrix3f  m1;
+//  m1 << 1, 2, 3,
+//    4, 5, 6,
+//    7, 8, 9;
+//
+//  m1.block<0,1> = (19,9,15);
+//
+//  cout << m1 <<endl;
+//
+//  return 0;
+}
--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,9 +1,15 @@
 #include "sphere.h"
 #include <iostream>
+#include <math.h>
+
+using namespace Eigen;
+using namespace std;
+
+//https://stackoverflow.com/questions/7840429/calculate-the-xyz-point-of-a-sphere-given-a-uv-coordinate-of-its-texture
 
 void sphere(
-  const int num_faces_u,
-  const int num_faces_v,
+  const int num_faces_u, // vertical line num longitudinal  xx
+  const int num_faces_v, // horizontal line num latitudinal yy
   Eigen::MatrixXd & V,
   Eigen::MatrixXi & F,
   Eigen::MatrixXd & UV,
@@ -11,7 +17,52 @@ void sphere(
   Eigen::MatrixXd & NV,
   Eigen::MatrixXi & NF)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
+  V.resize((num_faces_u+1)*(num_faces_v+1), 3);  // #V by 3
+  F.resize(num_faces_u*num_faces_v, 4);          // #F by 4
+  UV.resize((num_faces_u+1)*(num_faces_v+1), 2); // #V by 3
+  UF.resize(num_faces_u*num_faces_v, 4);         // #F by 4
+  // TODO following dim correct? why!!!!!!
+  NV.resize((num_faces_u+1)*(num_faces_v+1), 3);         // #F by 3
+  NF.resize(num_faces_u*num_faces_v, 4);         // #F by 4
+
+  double xLen=1.0/num_faces_u, yLen=1.0/num_faces_v;
+  double radius=1;
+
+  int cur=0;
+  for (int i=0;i<=num_faces_v;i++){
+    for (int j=0;j<=num_faces_u;j++){
+      double u = j*xLen, v=i*yLen;
+
+      double the = (2.0*M_PI)*u;
+      double phi = (M_PI)*v;
+
+      double x = cos(the) * sin(phi) * radius;
+      double y = sin(the) * sin(phi) * radius;
+      double z = cos(phi) * radius;
+
+      V.row(cur) << x,y,z;
+      UV.row(cur) << u,v;
+      NV.row(cur) << x,y,z; //center is 0,0,0
+
+      cur++;
+    }
+  }
+
+  cur=0;
+  for (int i=0;i<num_faces_v;i++){
+    for (int j=0;j<num_faces_u;j++){
+
+      int topLeft = i*(num_faces_u+1)+j;
+      int topRight = topLeft+1;
+      int botRight = (i+1)*(num_faces_u+1)+(j+1);
+      int botLeft = botRight-1;
+      F.row(cur) << topLeft, topRight, botRight, botLeft;
+      UF.row(cur) << topLeft, topRight, botRight, botLeft;
+      NF.row(cur) << topLeft, topRight, botRight, botLeft;
+
+      cur++;
+    }
+  }
+
+
 }
